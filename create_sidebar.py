34e4168da63745a6952efd5e0e1c2a763619b2c8
lovelaced <erin@parity.io>
2023-04-25 15:36:21 +0100

    adjust sidebar formatting

--- create_sidebar.py
+++ create_sidebar.py
@@ -1,16 +1,21 @@
 import os
 
 def create_sidebar(parent, sidebar, level=0):
+    indent = '  ' * level
+
     for item in sorted(os.listdir(parent)):
         path = os.path.join(parent, item)
         if os.path.isdir(path):
             if level == 0:
-                sidebar.write(f'▪ {item.upper()}\n\n')
+                sidebar.write(f'{indent}**{item.upper()}**\n\n')
             else:
-                sidebar.write(f'{level * " "} - {item}\n\n')
+                sidebar.write(f'{indent}- {item}\n')
             create_sidebar(path, sidebar, level + 1)
         elif item.endswith('.md') and item != '_Sidebar.md':
-            sidebar.write(f'{level * " "} - [{item[:-3]}]({parent.replace("wiki/", "")}/{item})\n')
+            sidebar.write(f'{indent}- [{item[:-3]}]({parent.replace("wiki/", "")}/{item})\n')
+
+    if level == 0:
+        sidebar.write('\n')
 
 parent = 'wiki'
 with open(os.path.join(parent, '_Sidebar.md'), 'w') as sidebar:
